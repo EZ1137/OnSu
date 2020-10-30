3875986751da72af4e5663604b7665a5fccc36ac
user <user@LAPTOP-VUVJMCGH>
2020-10-30 20:51:29 +0900

    승민 공지사항 작업완료(css빼고)

--- src/main/java/com/kh/onsoo/notice/controller/NoticeController.java
+++ src/main/java/com/kh/onsoo/notice/controller/NoticeController.java
@@ -1,5 +1,94 @@
 package com.kh.onsoo.notice.controller;
 
-public class NoticeController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.kh.onsoo.notice.model.biz.NoticeBiz;
+import com.kh.onsoo.notice.model.dto.NoticeDto;
+
+@Controller
+public class NoticeController {
+	
+	private Logger logger = LoggerFactory.getLogger(NoticeController.class);
+	
+	@Autowired
+	private NoticeBiz noticeBiz;
+	
+	//글 보기
+	@RequestMapping(value = "/notice.do")
+	public String selectList(Model model) {
+		model.addAttribute("notice",noticeBiz.selectList());
+		return "notice";
+	}
+	
+	
+	//글작성
+	@RequestMapping(value = "/noticeForm.do")
+	public String insertForm() {
+		return "noticeinsert";
+	}
+	
+	@RequestMapping(value = "/noticeRes.do")
+	public String noticeinsertRes(NoticeDto dto , Model model) {
+		
+		int res = noticeBiz.insert(dto);
+		
+		if(res>0) {
+			return "redirect: notice.do";
+		}else {
+			return "redirect: noticeForm.do";
+		}
+		
+	}
+	
+	
+	
+	//상세보기 
+	@RequestMapping(value = "/noticedetail.do")
+	public String selectOne(Model model, @RequestParam("notice_no") int notice_no) {
+		
+		model.addAttribute("dto",noticeBiz.selectOne(notice_no));
+		return "noticedetail";
+	}
+	
+	
+	
+	//수정 
+	@RequestMapping(value ="/noticeUpdate.do")
+	public String updateform(Model model, int notice_no) {
+		model.addAttribute("dto",noticeBiz.selectOne(notice_no));
+		return "noticeupdate";
+	}
+	@RequestMapping(value ="/updateRes.do")
+	public String updateRes(NoticeDto dto) {
+		int res = noticeBiz.update(dto);
+		
+		if(res>0) {
+			return "redirect: noticedetail.do?notice_no="+dto.getNotice_no();
+		}else {
+			return "redirect: noticeUpdate.do?notice_no="+dto.getNotice_no();
+		}
+	}
+	
+	
+	//삭제 
+	@RequestMapping(value ="/noticedelete.do" )
+	public String delete (int notice_no) {
+		int res = noticeBiz.delete(notice_no);
+		if(res>0) {
+			return "redirect: notice.do"; 
+		}else {
+			return "noticedetail.do?notice_seq="+notice_no;
+		}
+		
+	}
+	
+	
+	
+	
 }
---
+++ src/main/webapp/WEB-INF/views/notice.jsp
@@ -0,0 +1,79 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<link href="${pageContext.request.contextPath}/resources/css/notice.css" rel="stylesheet" >
+<body>
+
+<!-- header -->
+	<header id="header">
+		<%@ include file="/WEB-INF/views/header.jsp"%>
+	</header>
+
+
+
+<section class="section">
+			<h1 id="qnatitle">공지사항페이지입니다</h1>
+		<form id="notice" action="" method="post">
+			<input type="hidden" name="" value=""/>
+			<table  class="" style="text-align: center;">
+				<col width="100"/>
+				<col width="150"/>
+				<col width="300"/>
+				<col width="250"/>
+				<tr>
+					<th scope="col" class="">번호</th>
+					<th scope="col" class="">작성자</th>
+					<th scope="col" class="">제목</th>
+					<th scope="col" class="">작성일</th>
+				</tr>
+				<c:choose>
+					<c:when test="${empty notice}">
+						<tr>
+							<th colspan="4">-------------------------------------- 작성된 글이 없습니다.-----------------------------------</th>
+						</tr>
+					</c:when>
+				<c:otherwise>
+					<c:forEach items="${notice }" var="dto">
+						<tr>
+							<td>${dto.notice_no}</td>
+							<td>관리자</td>
+							<td><a href="noticedetail.do?notice_no=${dto.notice_no}" style="color:black;"> ${dto.notice_title}</a></td>
+							<td>${dto.notice_date}</td>
+						</tr>
+					</c:forEach>
+				</c:otherwise>
+				</c:choose>
+					<tr>
+						<td colspan="4" align="center">
+							<select name="">
+								<option value="" selected="selected">제목으로검색</option>
+								<option value="">내용으로 검색</option>
+							</select>
+							<input type="search" name="" placeholder="검색어를 입력하세요"/>
+							<button class="" type="submit">검색</button>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="4" align="right">
+							<input type="button" value="글작성" onclick="location.href='noticeForm.do'"/>
+						</td>
+					</tr>
+			</table>
+		</form>			
+	</section>
+	
+	<!-- footer -->
+	<footer id="footerarea" class="area">
+		<%@ include file="/WEB-INF/views/footer.jsp"%>
+	</footer>
+
+
+
+</body>
+</html>
